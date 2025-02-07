af385e2d73db41072910ceade8743fb92f0a8cd7
Sampreetha Shivalingu <sshival1@asu.edu>
2025-02-07 16:37:02 -0700

    initial code for deployment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quizapp3ba04c27": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
